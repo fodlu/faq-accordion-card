5abac5a27dc3e26360f7af5f10532119e5d599f2
Fodlu_dev <66522961+fodlu@users.noreply.github.com>
2023-05-05 11:55:30 +0000

    update on the script to avoid junks

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const question = document.querySelectorAll(".question");
 const answer = document.querySelectorAll(".answer");
+const arrow = document.querySelectorAll(".arrow");
 
-for (let i = 0; i < question.length; i++) {
-  question[i].addEventListener("click", () => {
-    question.forEach((a) => {
-      a.style.fontWeight = "400";
-    });
-    question[i].style.fontWeight = "700";
-    answer.forEach((a) => {
-      a.classList.remove("active");
-    });
-    answer[i].classList.toggle("active");
-  });
-}
+question.forEach((que, index) => {
+  que.addEventListener("click", (e) => {
+    const num = e.target;
 
-/* for (let i = 0; i < question.length; i++) {
-  question[i].addEventListener("click", () => {
-    answer.forEach((a) => {
-      a.style.display = "none";
-    });
-    answer[i].style.display = "block";
+    if (que.classList.contains("active")) {
+      que.style.transition = ".4s ease-in-out";
+      que.classList.remove("active");
+      arrow.forEach((arr) => (arr.style.transform = "rotate(0deg)"));
+      answer.forEach((a) => a.classList.remove("active"));
+    } else {
+      question.forEach((q) => {
+        q.classList.remove("active");
+      });
+      que.classList.toggle("active");
+
+      answer.forEach((a) => a.classList.remove("active"));
+      answer[index].classList.toggle("active");
+
+      arrow.forEach((arr) => (arr.style.transform = "rotate(0deg)"));
+      arrow[index].style.transform = "rotate(180deg)";
+    }
   });
-}
- */
+});
